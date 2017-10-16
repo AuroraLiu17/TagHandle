4129f263fad6e0d3b1572566b47dfbb36df8d117
AuroraLiu17 <1473375730@qq.com>
2017-10-16 14:38:51 +0800

    Format code, create export sub folders if not exist

--- src/extractor/TagHandleHelper.java
+++ src/extractor/TagHandleHelper.java
@@ -21,189 +21,199 @@ import utils.RowDataReader.ReaderCallback;
 import utils.TextUtils;
 
 public class TagHandleHelper {
-	
-	private static final String RESOURCE_IMAGE_FOLDER_NAME = "image";
-	private static final String RESOURCE_TAG_FOLDER_NAME = "label";
-	private static final String RESOURCE_TAG_FILE_EXTENTION = ".tag";
-	private static final String EXPORT_EXCEL_FILE_NAME = "test_user.xls";
-	
-	public static boolean doExtract(String resourceFolderPath, 
-			String exportFolderPath, String tagListFilePath) throws Exception {
-		Map<String, Integer> tags = readTags(FileUtils.getFile(tagListFilePath, true));
-		if (tags == null || tags.isEmpty()) {
-			System.out.println("Nothing to extract, empty tag list");
-			return false;
-		}
-		System.out.println("On extract tags " + tags.toString());
-		if (!resourceFolderPath.endsWith("/")) {
-			resourceFolderPath += "/";
-		}
-		if (!exportFolderPath.endsWith("/")) {
-			exportFolderPath += "/";
-		}
-		String resourceImageFolderPath = resourceFolderPath + RESOURCE_IMAGE_FOLDER_NAME;
-		String resourceTagFolderPath = resourceFolderPath + RESOURCE_TAG_FOLDER_NAME;
-		File resourceImageFolder = FileUtils.getFolder(resourceImageFolderPath, true);
-		File resourceTagFolder = FileUtils.getFolder(resourceTagFolderPath, true);
-		extractFromFolder(tags, resourceImageFolder, resourceTagFolder, exportFolderPath);
-		System.out.println("Done extracting, result:" + tags.toString());
-		return true;
-	}
-	
-	private static final int COLUMN_INDEX_IMAGE_FILE_NAME = 0;
-	private static final int COLUMN_INDEX_CREATE_TIME = 1;
-	private static final int COLUMN_INDEX_GPS = 2;
-	private static final int COLUMN_INDEX_LOCATION = 3;
-	private static final int COLUMN_INDEX_TAG_INFO = 4;
-	
-	public static boolean generateExcel(String resourceFolderPath) 
-			throws Exception {
-		String resourceImageFolderPath = resourceFolderPath + "/" + RESOURCE_IMAGE_FOLDER_NAME;
-		String resourceTagFolderPath = resourceFolderPath + "/" + RESOURCE_TAG_FOLDER_NAME;
-		File resourceImageFolder = FileUtils.getFolder(resourceImageFolderPath, true);
-		File resourceTagFolder = FileUtils.getFolder(resourceTagFolderPath, true);
-		
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		HSSFSheet sheet = workbook.createSheet("sheet1");
-		
-		int rowNum = 0;
-		for (File imageFile : resourceImageFolder.listFiles()) {
-			if (imageFile.isDirectory()) {
-				continue;
-			}
-			File tagFile = getTagFileByImageFile(imageFile, resourceTagFolder);
-			if (tagFile == null) {
-				System.out.println("Invalid tag file " + imageFile.getAbsolutePath());
-				continue;
-			}
-			HSSFRow row = sheet.createRow(rowNum);
-			row.createCell(COLUMN_INDEX_IMAGE_FILE_NAME, CellType.STRING).setCellValue(imageFile.getName());
-			String tagInfo = formatTagFile(FileUtils.readFileToString(tagFile));
-			row.createCell(COLUMN_INDEX_TAG_INFO, CellType.STRING).setCellValue(tagInfo);;
-			rowNum++;
-		}
-		
-		File exportFile = new File(resourceFolderPath, EXPORT_EXCEL_FILE_NAME);
-		workbook.write(exportFile);
-		workbook.close();
-		return true;
-	}
-	
-	private static void extractFromFolder(Map<String, Integer> extractTags,
-			File resourceImageFolder, File resourceTagFolder, String exportFolderPath) {
-		System.out.println(String.format("Extract from [%s], [%s] to [%s]", resourceImageFolder.getAbsolutePath(), resourceTagFolder.getAbsolutePath(), exportFolderPath));
-		for (File imageFile : resourceImageFolder.listFiles()) {
-			if (imageFile.isDirectory()) {
-				File subTagFolder = new File(resourceTagFolder, imageFile.getName());
-				extractFromFolder(extractTags, imageFile, subTagFolder, exportFolderPath);
-				continue;
-			}
-			File tagFile = getTagFileByImageFile(imageFile, resourceTagFolder);
-			if (tagFile == null) {
-				System.out.println("Invalid tag file " + imageFile.getAbsolutePath());
-				continue;
-			}
-			List<String> imageTags = readImageTagFile(tagFile);
-			if (imageTags == null || imageTags.size() <= 0) {
-				continue;
-			}
-			
-			for (String imageTag : imageTags) {
-				boolean copied = false;
-				Integer count = extractTags.get(imageTag);
-				if (count == null) {
-					continue;
-				}
-				if (!copied) {
-					System.out.println(String.format("Hit image [%s]", imageFile.getAbsolutePath()));
-					FileUtils.copyFile(imageFile, new File(exportFolderPath + RESOURCE_IMAGE_FOLDER_NAME, imageFile.getName()));
-					FileUtils.copyFile(tagFile, new File(exportFolderPath + RESOURCE_TAG_FOLDER_NAME, tagFile.getName()));
-					copied = true;
-				}
-				extractTags.put(imageTag, count + 1);
-			}
-		}
-	}
-	
-	private static File getTagFileByImageFile(File imageFile, File tagParentFolder) {
-		String tagFileName = getTagFileNameByImageName(imageFile.getName());
-		if (TextUtils.isEmpty(tagFileName)) {
-			System.out.println("Invalid tag file name");
-			return null;
-		}
-		File tagFile = new File(tagParentFolder, tagFileName);
-		return tagFile.exists() ? tagFile : null;
-	}
-	
-	private static String getTagFileNameByImageName(String imageName) {
-		if (TextUtils.isEmpty(imageName)) {
-			return null;
-		}
-		String fileName = FileUtils.getFileNameWithoutExtension(imageName);
-		String extension = FileUtils.getExtension(imageName);
-		if (!TextUtils.isEmpty(extension)) {
-			return fileName + "_" + extension + RESOURCE_TAG_FILE_EXTENTION;
-		} else {
-			return fileName + RESOURCE_TAG_FILE_EXTENTION;
-		}
-	}
-	
-	private static String formatTagFile(String data) {
-		if (!TextUtils.isEmpty(data)) {
-			data = data.replace("#", "");
-			data = data.replace("@", "");
-		}
-		return data;
-	}
-	
-	private static final Gson S_GSON = new Gson();
-	private static List<String> readImageTagFile(File tagFile) {
-		try {
-			String jsonData = formatTagFile(FileUtils.readFileToString(tagFile));
-			Type type = new TypeToken<Map<String, String>>() {}.getType();  
-		    Map<String, String> tagMap = S_GSON.fromJson(jsonData, type);
-		    if (tagMap == null || tagMap.isEmpty()) {
-				return null;
-			}
 
-			final List<String> tags = new ArrayList<>();
-			for (String tagInfo : tagMap.values()) {
-				if (!TextUtils.isEmpty(tagInfo)) {
-					String[] subTagInfo = tagInfo.split(",");
-					if (subTagInfo != null) {
-						for (String subTag : subTagInfo) {
-							tags.add(subTag);
-						}
-					}
-				}
-			}
-			return tags;
-		} catch (Exception e) {
-			System.err.println("Failed read image tags");
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	private static Map<String, Integer> readTags(File tagListFile) {
-		if (tagListFile == null) {
-			return null;
-		}
-		
-		final Map<String, Integer> tags = new HashMap<>();
-		ReaderCallback callback = new ReaderCallback() {
-			@Override
-			public boolean onReadRow(int rowIndex, String rowData) {
-				if (!TextUtils.isEmpty(rowData)) {
-					if (rowIndex <= 1 && rowData.startsWith("\uFEFF")) {
-						rowData = rowData.substring(1);
-					}
-					tags.put(rowData.trim(), 0);
-				}
-				return true;
-			}
-		};
-		RowDataReader.read(tagListFile, callback);
-		return tags;
-	}
+    private static final String RESOURCE_IMAGE_FOLDER_NAME = "image";
+    private static final String RESOURCE_TAG_FOLDER_NAME = "label";
+    private static final String RESOURCE_TAG_FILE_EXTENSION = ".tag";
+    private static final String EXPORT_EXCEL_FILE_NAME = "test_user.xls";
+
+    public static boolean doExtract(String resourceFolderPath,
+                                    String exportFolderPath, String tagListFilePath) throws Exception {
+        Map<String, Integer> tags = readTags(FileUtils.getFile(tagListFilePath, true));
+        if (tags == null || tags.isEmpty()) {
+            System.out.println("Nothing to extract, empty tag list");
+            return false;
+        }
+        System.out.println("On extract tags " + tags.toString());
+        if (!resourceFolderPath.endsWith("/")) {
+            resourceFolderPath += "/";
+        }
+        if (!exportFolderPath.endsWith("/")) {
+            exportFolderPath += "/";
+        }
+        if (!new File(exportFolderPath + RESOURCE_IMAGE_FOLDER_NAME).exists()) {
+            FileUtils.createFolder(exportFolderPath + RESOURCE_IMAGE_FOLDER_NAME);
+        }
+        if (!new File(exportFolderPath + RESOURCE_TAG_FOLDER_NAME).exists()) {
+            FileUtils.createFolder(exportFolderPath + RESOURCE_TAG_FOLDER_NAME);
+        }
+
+        String resourceImageFolderPath = resourceFolderPath + RESOURCE_IMAGE_FOLDER_NAME;
+        String resourceTagFolderPath = resourceFolderPath + RESOURCE_TAG_FOLDER_NAME;
+        File resourceImageFolder = FileUtils.getFolder(resourceImageFolderPath, true);
+        File resourceTagFolder = FileUtils.getFolder(resourceTagFolderPath, true);
+        extractFromFolder(tags, resourceImageFolder, resourceTagFolder, exportFolderPath);
+        System.out.println("Done extracting, result:" + tags.toString());
+        return true;
+    }
+
+    private static final int COLUMN_INDEX_IMAGE_FILE_NAME = 0;
+    private static final int COLUMN_INDEX_CREATE_TIME = 1;
+    private static final int COLUMN_INDEX_GPS = 2;
+    private static final int COLUMN_INDEX_LOCATION = 3;
+    private static final int COLUMN_INDEX_TAG_INFO = 4;
+
+    public static boolean generateExcel(String resourceFolderPath)
+            throws Exception {
+        String resourceImageFolderPath = resourceFolderPath + "/" + RESOURCE_IMAGE_FOLDER_NAME;
+        String resourceTagFolderPath = resourceFolderPath + "/" + RESOURCE_TAG_FOLDER_NAME;
+        File resourceImageFolder = FileUtils.getFolder(resourceImageFolderPath, true);
+        File resourceTagFolder = FileUtils.getFolder(resourceTagFolderPath, true);
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("sheet1");
+
+        int rowNum = 0;
+        for (File imageFile : resourceImageFolder.listFiles()) {
+            if (imageFile.isDirectory()) {
+                continue;
+            }
+            File tagFile = getTagFileByImageFile(imageFile, resourceTagFolder);
+            if (tagFile == null) {
+                System.out.println("Invalid tag file " + imageFile.getAbsolutePath());
+                continue;
+            }
+            HSSFRow row = sheet.createRow(rowNum);
+            row.createCell(COLUMN_INDEX_IMAGE_FILE_NAME, CellType.STRING).setCellValue(imageFile.getName());
+            String tagInfo = formatTagFile(FileUtils.readFileToString(tagFile));
+            row.createCell(COLUMN_INDEX_TAG_INFO, CellType.STRING).setCellValue(tagInfo);
+            ;
+            rowNum++;
+        }
+
+        File exportFile = new File(resourceFolderPath, EXPORT_EXCEL_FILE_NAME);
+        workbook.write(exportFile);
+        workbook.close();
+        return true;
+    }
+
+    private static void extractFromFolder(Map<String, Integer> extractTags,
+                                          File resourceImageFolder, File resourceTagFolder, String exportFolderPath) {
+        System.out.println(String.format("Extract from [%s], [%s] to [%s]", resourceImageFolder.getAbsolutePath(), resourceTagFolder.getAbsolutePath(), exportFolderPath));
+        for (File imageFile : resourceImageFolder.listFiles()) {
+            if (imageFile.isDirectory()) {
+                File subTagFolder = new File(resourceTagFolder, imageFile.getName());
+                extractFromFolder(extractTags, imageFile, subTagFolder, exportFolderPath);
+                continue;
+            }
+            File tagFile = getTagFileByImageFile(imageFile, resourceTagFolder);
+            if (tagFile == null) {
+                System.out.println("Invalid tag file " + imageFile.getAbsolutePath());
+                continue;
+            }
+            List<String> imageTags = readImageTagFile(tagFile);
+            if (imageTags == null || imageTags.size() <= 0) {
+                continue;
+            }
+
+            for (String imageTag : imageTags) {
+                boolean copied = false;
+                Integer count = extractTags.get(imageTag);
+                if (count == null) {
+                    continue;
+                }
+                if (!copied) {
+                    System.out.println(String.format("Hit image [%s]", imageFile.getAbsolutePath()));
+                    FileUtils.copyFile(imageFile, new File(exportFolderPath + RESOURCE_IMAGE_FOLDER_NAME, imageFile.getName()));
+                    FileUtils.copyFile(tagFile, new File(exportFolderPath + RESOURCE_TAG_FOLDER_NAME, tagFile.getName()));
+                    copied = true;
+                }
+                extractTags.put(imageTag, count + 1);
+            }
+        }
+    }
+
+    private static File getTagFileByImageFile(File imageFile, File tagParentFolder) {
+        String tagFileName = getTagFileNameByImageName(imageFile.getName());
+        if (TextUtils.isEmpty(tagFileName)) {
+            System.out.println("Invalid tag file name");
+            return null;
+        }
+        File tagFile = new File(tagParentFolder, tagFileName);
+        return tagFile.exists() ? tagFile : null;
+    }
+
+    private static String getTagFileNameByImageName(String imageName) {
+        if (TextUtils.isEmpty(imageName)) {
+            return null;
+        }
+        String fileName = FileUtils.getFileNameWithoutExtension(imageName);
+        String extension = FileUtils.getExtension(imageName);
+        if (!TextUtils.isEmpty(extension)) {
+            return fileName + "_" + extension + RESOURCE_TAG_FILE_EXTENSION;
+        } else {
+            return fileName + RESOURCE_TAG_FILE_EXTENSION;
+        }
+    }
+
+    private static String formatTagFile(String data) {
+        if (!TextUtils.isEmpty(data)) {
+            data = data.replace("#", "");
+            data = data.replace("@", "");
+        }
+        return data;
+    }
+
+    private static final Gson S_GSON = new Gson();
+
+    private static List<String> readImageTagFile(File tagFile) {
+        try {
+            String jsonData = formatTagFile(FileUtils.readFileToString(tagFile));
+            Type type = new TypeToken<Map<String, String>>() {
+            }.getType();
+            Map<String, String> tagMap = S_GSON.fromJson(jsonData, type);
+            if (tagMap == null || tagMap.isEmpty()) {
+                return null;
+            }
+
+            final List<String> tags = new ArrayList<>();
+            for (String tagInfo : tagMap.values()) {
+                if (!TextUtils.isEmpty(tagInfo)) {
+                    String[] subTagInfo = tagInfo.split(",");
+                    if (subTagInfo != null) {
+                        for (String subTag : subTagInfo) {
+                            tags.add(subTag);
+                        }
+                    }
+                }
+            }
+            return tags;
+        } catch (Exception e) {
+            System.err.println("Failed read image tags");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static Map<String, Integer> readTags(File tagListFile) {
+        if (tagListFile == null) {
+            return null;
+        }
+
+        final Map<String, Integer> tags = new HashMap<>();
+        ReaderCallback callback = new ReaderCallback() {
+            @Override
+            public boolean onReadRow(int rowIndex, String rowData) {
+                if (!TextUtils.isEmpty(rowData)) {
+                    if (rowIndex <= 1 && rowData.startsWith("\uFEFF")) {
+                        rowData = rowData.substring(1);
+                    }
+                    tags.put(rowData.trim(), 0);
+                }
+                return true;
+            }
+        };
+        RowDataReader.read(tagListFile, callback);
+        return tags;
+    }
 }
--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -9,163 +9,202 @@ import utils.RowDataReader.ReaderCallback;
 
 public class FileUtils {
 
-	public static String readFileToString(File file) {
-		if (file == null) {
-			System.err.println("Invalid file");
-			return null;
-		}
+    private static final int READ_BUFFER_SIZE = 1444;
 
-		final StringBuffer stringBuffer = new StringBuffer();
-		ReaderCallback callback = new ReaderCallback() {
-			@Override
-			public boolean onReadRow(int rowIndex, String rowData) {
-				stringBuffer.append(rowData).append("\n");
-				return true;
-			}
-		};
-		RowDataReader.read(file, callback);
+    public static String readFileToString(File file) {
+        if (file == null) {
+            System.err.println("Invalid file");
+            return null;
+        }
 
-		int length = stringBuffer.length();
-		if (length > 0) {
-			stringBuffer.delete(length - 1, length);
-		}
-		return stringBuffer.toString();
-	}
+        final StringBuffer stringBuffer = new StringBuffer();
+        ReaderCallback callback = new ReaderCallback() {
+            @Override
+            public boolean onReadRow(int rowIndex, String rowData) {
+                stringBuffer.append(rowData).append("\n");
+                return true;
+            }
+        };
+        RowDataReader.read(file, callback);
 
-	public static String readFileToString(String filePath) {
-		if (TextUtils.isEmpty(filePath)) {
-			System.err.println("Invalid path");
-			return null;
-		}
-		return readFileToString(new File(filePath));
-	}
+        int length = stringBuffer.length();
+        if (length > 0) {
+            stringBuffer.delete(length - 1, length);
+        }
+        return stringBuffer.toString();
+    }
 
-	public static String getExtension(String filePath) {
-		if (!TextUtils.isEmpty(filePath)) {
-			int index = filePath.lastIndexOf('.');
-			return index > -1 ? filePath.substring(index + 1) : null;
-		}
-		return null;
-	}
+    public static String readFileToString(String filePath) {
+        if (TextUtils.isEmpty(filePath)) {
+            System.err.println("Invalid path");
+            return null;
+        }
+        return readFileToString(new File(filePath));
+    }
 
-	public static String getFileNameWithoutExtension(String filePath) {
-		if (!TextUtils.isEmpty(filePath)) {
-			int extensionIndex = filePath.lastIndexOf('.');
-			extensionIndex = extensionIndex > -1 ? extensionIndex : filePath.length();
-			int pathSeparatorIndex = filePath.lastIndexOf('/');
-			pathSeparatorIndex = pathSeparatorIndex > -1 ? pathSeparatorIndex : -1;
-			return filePath.substring(pathSeparatorIndex + 1, extensionIndex);
-		}
-		return null;
-	}
+    public static String getExtension(String filePath) {
+        if (!TextUtils.isEmpty(filePath)) {
+            int index = filePath.lastIndexOf('.');
+            return index > -1 ? filePath.substring(index + 1) : null;
+        }
+        return null;
+    }
 
-	public static File getFile(String path) throws Exception {
-		return getFile(path, false);
-	}
+    public static String getFileNameWithoutExtension(String filePath) {
+        if (!TextUtils.isEmpty(filePath)) {
+            int extensionIndex = filePath.lastIndexOf('.');
+            extensionIndex = extensionIndex > -1 ? extensionIndex : filePath.length();
+            int pathSeparatorIndex = filePath.lastIndexOf('/');
+            pathSeparatorIndex = pathSeparatorIndex > -1 ? pathSeparatorIndex : -1;
+            return filePath.substring(pathSeparatorIndex + 1, extensionIndex);
+        }
+        return null;
+    }
 
-	public static File getFile(String path, boolean checkExistance) throws Exception {
-		if (TextUtils.isEmpty(path)) {
-			throw new IllegalArgumentException("Invalid input paths");
-		}
-		File file = new File(path);
-		if (checkExistance && !file.exists()) {
-			throw new FileNotFoundException("File " + path + " do not exist");
-		}
-		return file;
-	}
+    public static File getFile(String path) throws Exception {
+        return getFile(path, false);
+    }
 
-	public static File getFolder(String path) throws Exception {
-		return getFolder(path, false);
-	}
+    public static File getFile(String path, boolean checkExistance) throws Exception {
+        if (TextUtils.isEmpty(path)) {
+            throw new IllegalArgumentException("Invalid input paths");
+        }
+        File file = new File(path);
+        if (checkExistance && !file.exists()) {
+            throw new FileNotFoundException("File " + path + " do not exist");
+        }
+        return file;
+    }
 
-	public static File getFolder(String path, boolean checkExistance) throws Exception {
-		File folder = getFile(path, checkExistance);
-		return folder.isDirectory() ? folder : null;
-	}
+    public static File getFolder(String path) throws Exception {
+        return getFolder(path, false);
+    }
 
-	public static boolean deleteFile(String filePath) {
-		if (TextUtils.isEmpty(filePath)) {
-			System.err.println("Invalid path");
-			return false;
-		}
-		return deleteFile(new File(filePath));
-	}
+    public static File getFolder(String path, boolean checkExistence) throws Exception {
+        File folder = getFile(path, checkExistence);
+        return folder.isDirectory() ? folder : null;
+    }
 
-	public static boolean deleteFile(File file) {
-		if (file == null) {
-			System.err.println("Invalid file");
-			return false;
-		}
-		try {
-			return file.delete();
-		} catch (Exception e) {
-			System.err.println("Failed when deleting file ");
-			e.printStackTrace(System.err);
-			return false;
-		}
-	}
+    public static boolean deleteFile(String filePath) {
+        if (TextUtils.isEmpty(filePath)) {
+            System.err.println("Invalid path");
+            return false;
+        }
+        return deleteFile(new File(filePath));
+    }
 
-	public static boolean moveFile(String srcPath, String desPath) {
-		if (TextUtils.isEmpty(srcPath) || TextUtils.isEmpty(desPath)) {
-			System.err.println("Invalid paths");
-			return false;
-		}
-		return moveFile(new File(srcPath), new File(desPath));
-	}
+    public static boolean deleteFile(File file) {
+        if (file == null) {
+            System.err.println("Invalid file");
+            return false;
+        }
+        try {
+            return file.delete();
+        } catch (Exception e) {
+            System.err.println("Failed when deleting file ");
+            e.printStackTrace(System.err);
+            return false;
+        }
+    }
 
-	public static boolean moveFile(File srcFile, File desFile) {
-		if (srcFile == null || desFile == null) {
-			System.err.println("Invalid file");
-			return false;
-		}
-		try {
-			return srcFile.renameTo(desFile);
-		} catch (Exception e) {
-			System.err.println("Failed when moving file ");
-			e.printStackTrace(System.err);
-			return false;
-		}
-	}
+    public static boolean moveFile(String srcPath, String desPath) {
+        if (TextUtils.isEmpty(srcPath) || TextUtils.isEmpty(desPath)) {
+            System.err.println("Invalid paths");
+            return false;
+        }
+        return moveFile(new File(srcPath), new File(desPath));
+    }
 
-	private static final int READ_BUFFER_SIZE = 1444;
+    public static boolean moveFile(File srcFile, File desFile) {
+        if (srcFile == null || desFile == null) {
+            System.err.println("Invalid file");
+            return false;
+        }
+        try {
+            return srcFile.renameTo(desFile);
+        } catch (Exception e) {
+            System.err.println("Failed when moving file ");
+            e.printStackTrace(System.err);
+            return false;
+        }
+    }
 
-	public static boolean copyFile(String srcPath, String desPath) {
-		if (TextUtils.isEmpty(srcPath) || TextUtils.isEmpty(desPath)) {
-			System.err.println("Invalid paths");
-			return false;
-		}
-		return copyFile(new File(srcPath), new File(desPath));
-	}
+    public static boolean copyFile(String srcPath, String desPath) {
+        if (TextUtils.isEmpty(srcPath) || TextUtils.isEmpty(desPath)) {
+            System.err.println("Invalid paths");
+            return false;
+        }
+        return copyFile(new File(srcPath), new File(desPath));
+    }
 
-	public static boolean copyFile(File srcFile, File desFile) {
-		if (srcFile == null || desFile == null) {
-			System.err.println("Invalid file");
-			return false;
-		}
-		FileInputStream inputStream = null;
-		FileOutputStream outputStream = null;
-		try {
-			int byteread = 0;
-			if (!srcFile.exists()) {
-				System.err.println("Source file don't exist!");
-				return false;
-			}
-			inputStream = new FileInputStream(srcFile);
-			outputStream = new FileOutputStream(desFile);
-			byte[] buffer = new byte[READ_BUFFER_SIZE];
-			while ((byteread = inputStream.read(buffer)) != -1) {
-				outputStream.write(buffer, 0, byteread);
-			}
-			System.out.println(String.format("Done copying file from [%s] to [%s]", srcFile.getAbsolutePath(), desFile.getAbsolutePath()));
-			return true;
-		} catch (Exception e) {
-			System.err.println("Failed when copying file ");
-			e.printStackTrace(System.err);
-			deleteFile(desFile);
-			return false;
-		} finally {
-			MiscUtils.closeSilently(inputStream);
-			MiscUtils.closeSilently(outputStream);
-		}
-	}
+    public static boolean copyFile(File srcFile, File desFile) {
+        if (srcFile == null || desFile == null) {
+            System.err.println("Invalid file");
+            return false;
+        }
+        FileInputStream inputStream = null;
+        FileOutputStream outputStream = null;
+        try {
+            int byteread = 0;
+            if (!srcFile.exists()) {
+                System.err.println("Source file don't exist!");
+                return false;
+            }
+            inputStream = new FileInputStream(srcFile);
+            outputStream = new FileOutputStream(desFile);
+            byte[] buffer = new byte[READ_BUFFER_SIZE];
+            while ((byteread = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, byteread);
+            }
+            System.out.println(String.format("Done copying file from [%s] to [%s]", srcFile.getAbsolutePath(), desFile.getAbsolutePath()));
+            return true;
+        } catch (Exception e) {
+            System.err.println("Failed when copying file ");
+            e.printStackTrace(System.err);
+            deleteFile(desFile);
+            return false;
+        } finally {
+            MiscUtils.closeSilently(inputStream);
+            MiscUtils.closeSilently(outputStream);
+        }
+    }
+
+    public static boolean createFolder(String destFolder) {
+        File destFolderFile = new File(destFolder);
+        return createFolder(destFolderFile);
+    }
+
+    public static boolean createFolder(File destFolderFile) {
+        if (destFolderFile.exists() && destFolderFile.isFile()) {
+            boolean deleted = deleteFile(destFolderFile);
+            System.out.println("CreateFolder, there is a file that has the same name as the dest folder, " +
+                    "delete it first. result:" + deleted);
+            if (!deleted) {
+                return false;
+            }
+        }
+
+        // ensure the destination folder exist
+        mkdirsCustomize(destFolderFile);
+        if (!destFolderFile.exists()) {
+            System.out.println("folder: " + destFolderFile + " Can not be created.");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean mkdirsCustomize(File file) {
+        // If the terminal directory already exists, return is it a directory
+        if (file.exists()) {
+            return file.isDirectory();
+        }
+
+        // Try to create a parent directory and then this directory
+        if (!mkdirsCustomize(file.getParentFile())) {
+            return false;
+        }
+
+        // If the receiver can be created, answer true
+        return file.mkdir();
+    }
 }
